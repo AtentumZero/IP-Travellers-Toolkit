dc00b8a5daa94ae4d87beb1b860bd788a72f67ee
apacketofsweets <19573127+apacketofsweets@users.noreply.github.com>
2019-02-14 10:56:36 +0000

    Added a comment

--- modules/screenshot.py
+++ modules/screenshot.py
@@ -40,6 +40,7 @@ def get_security(TCP_IP):
     return snapshot_flag
 
 # Read IP file and takes a screenshot for each IP where no authentication is required
+# This script must be run from the workind directory in order for the below code to work
 
 if __name__ == '__main__':
     print "Attempting screenshots..."
@@ -48,7 +49,7 @@ if __name__ == '__main__':
 	    ip_addr = line.strip('\n')
             vncsnap_flag = get_security(ip_addr)
             CURRENT_INDEX = CURRENT_INDEX + 1
-            os.system("echo " + str(CURRENT_INDEX) + " > vnc-log.txt")
+            os.system("echo " + str(CURRENT_INDEX) + " > results/vnc-log.txt")
             if vncsnap_flag == 1:
                 CMD = "timeout 60 vncsnapshot -allowblank " + ip_addr + ":0 " + ip_addr + ".jpg > /dev/null 2>&1"
                 os.system(CMD)
