a1b046c45d857740224dedf2623302c3f249af25
apacketofsweets <19573127+apacketofsweets@users.noreply.github.com>
2019-05-02 13:18:27 +0100

    Added support for custom scan rates

--- modules/vnc.sh
+++ modules/vnc.sh
@@ -5,13 +5,17 @@ RESULTSDIR=$WORKINGDIR/results
 IPFILE=$RESULTSDIR/vnc-ips.txt
 TMP=$RESULTSDIR/vnc-tmp.txt
 SCANRESULTS=$RESULTSDIR/vnc-scan.txt
+EXCLUDE=$RESULTSDIR/exclude.txt
 PORT=5900
 
 echo "Please enter an IP address or range to scan..."
 read IP
 
+echo "Please enter a packet-per-second rate to scan with... (Slow: 10pps, Fast: 10000pps)"
+read RATE
+
 # Scans specified IP address(es) and outputs  to a file
-sudo masscan -p$PORT $IP >> $SCANRESULTS
+sudo masscan -p$PORT $IP --rate $RATE --excludefile $EXCLUDE >> $SCANRESULTS
 
 # Reads $SCANRESULTS, extracts IP addresses only and outputs to a file
 perl -lne 'print $& if /(\d+\.){3}\d+/' $SCANRESULTS > $IPFILE
